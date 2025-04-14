bea2108b1af20fab451839c6e8de898379036d72
FaiazKabir <69064034+FaiazKabir@users.noreply.github.com>
2025-04-14 19:34:48 +0600

    Delete postBuild.sh

--- postBuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Make sure docs directory exists
-mkdir -p docs
-
-# Convert notebook to HTML without code cells, and save directly to docs/index.html
-jupyter nbconvert provinceszipped.ipynb --no-input --to html --output docs/index
