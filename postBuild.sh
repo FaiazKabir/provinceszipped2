e447a59e53b86389539abaf4ac328e059ac4c33a
FaiazKabir <69064034+FaiazKabir@users.noreply.github.com>
2025-04-14 19:14:13 +0600

    Update postBuild.sh

--- postBuild.sh
+++ postBuild.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Convert notebook to HTML without code cells
-jupyter nbconvert provinceszipped.ipynb --no-input --to html --output docs/index.html
-
 # Make sure docs directory exists
 mkdir -p docs
-mv provinceszipped.html docs/index.html
+
+# Convert notebook to HTML without code cells, and save directly to docs/index.html
+jupyter nbconvert provinceszipped.ipynb --no-input --to html --output docs/index
